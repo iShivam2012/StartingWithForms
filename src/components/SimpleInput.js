12599f443df0d6e275b0f6935373a78d2ec18617
iShivam2012 <shivam.gupta97a@gmail.com>
2021-06-27 16:58:08 +0530

    adding basic validation and fetching user input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,29 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [userEntered, setUserEntered] = useState('')
+  const userEnteredRef = useRef();
+
+  const onChangeHandler = event => {
+    setUserEntered(event.target.value);
+  }
+
+  const onSubmitHandler = event => {
+    event.preventDefault();
+    if (userEntered.trim() === '') {
+      return;
+    }
+
+    const inputRef = userEnteredRef.current.value
+    console.log("inputRef " + inputRef)
+    console.log("inputState " + userEntered)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler} >
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={userEnteredRef} onChange={onChangeHandler} type='text' id='name' />
       </div>
       <div className="form-actions">
         <button>Submit</button>
